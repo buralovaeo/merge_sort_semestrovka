bab098b840eea8bc5cdb47c363cbfd38a8236f4d
buralovaeo <buralova.eo@gmail.com>
2026-04-24 16:10:48 +0300

    Update Main.java

--- MergeSort/Main.java
+++ MergeSort/Main.java
@@ -1,12 +1,1 @@
-package MergeSort;
 
-public class Main {
-    public static void main(String[] args) {
-        try {
-            Benchmark.main(args);
-        } catch (Exception e) {
-            System.err.println("Ошибка: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-}
